92249398a48401b66b9c0c310b0e8e009cc64426
Daniel Yang <daniel.yang.2000@utexas.edu>
2022-08-14 19:12:15 -0500

    comments and more modular design

--- RankScrape.py
+++ RankScrape.py
@@ -7,6 +7,7 @@ import pandas as pd
 # # url = 'https://www.atptour.com/en/rankings/singles?rankRange=1-5000'
 # url = 'https://www.atptour.com/en/rankings/singles'
 # url_doubles = 'https://www.atptour.com/en/rankings/doubles?rankRange=1-5000'
+root = 'www.atptour.com'
 
 def scrape_player(link):
     html_text = requests.get(link).text
@@ -18,6 +19,13 @@ def scrape_player(link):
     main = container.find('div', id='mainContainer')
     content = main.find('div', id='mainContent')
     player = content.find('div',id='playerProfileHero')
+    try:
+        image = player.find('div', class_='player-profile-hero-image')
+        image_url = image.find('img')['src']
+    except:
+        image_url = None
+
+
     player_prof = player.find('div', class_='player-profile-hero-overflow')
     table = player_prof.find('div', class_='player-profile-hero-table').find('div', class_='inner-wrap').find('table')
 
@@ -37,11 +45,42 @@ def scrape_player(link):
         height = height.text.strip()
         height = int(height[1:-3])
     
-    return (height, weight)
+    return (height, weight, image_url)
 
+def yield_rank(entry):
+    cells = entry.find_all('td')
+
+    player = cells[3]
+    player_info = player.find('span')
+    player_name = player_info.find('a').text.strip()
+    player_link = root + player_info.find('a')['href']
+    player_link = player_link.split('/')
+    player_link[-1] = 'player-stats'
+    player_link = 'http://' + '/'.join(player_link)
     
+    height, weight, image_url = scrape_player(player_link)
+    # print(player_link)
 
-def scrape_rank(url):
+    player_rank = cells[0].text.strip()
+
+    point_data = cells[5]
+    player_points = point_data.find('a').text.strip()
+    player_points = player_points.replace(',', '')
+    player_points = int(player_points)
+        
+
+    player_flag = cells[2].div.div.img['alt']
+
+    player_age = cells[4].text.strip()
+    try:
+        player_age = int(player_age)
+    except:
+        pass
+
+    # print(player_rank, player_name, player_points, player_link, player_age, player_flag, height, weight)
+    return [player_rank, player_name, str(player_points), str(player_age), player_flag, str(height), str(weight), player_link, image_url]
+
+def scrape_rank(url, command):
     # url = 'https://www.atptour.com/en/rankings/singles'
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'lxml')
@@ -55,45 +94,26 @@ def scrape_rank(url):
     megaTable = tablesRanking.find('table', class_='mega-table')
     tableBody = megaTable.find('tbody')
 
-    root = 'www.atptour.com'
     entries = tableBody.find_all('tr')
 
-    for i, entry in enumerate(entries):
-        if i == 10:
-            break
-        cells = entry.find_all('td')
-
-        player = cells[3]
-        player_info = player.find('span')
-        player_name = player_info.find('a').text.strip()
-        player_link = root + player_info.find('a')['href']
-        player_link = player_link.split('/')
-        player_link[-1] = 'player-stats'
-        player_link = 'http://' + '/'.join(player_link)
-        
-        height, weight = scrape_player(player_link)
-        # print(player_link)
-
-        player_rank = cells[0].text.strip()
-
-        point_data = cells[5]
-        player_points = point_data.find('a').text.strip()
-        player_points = player_points.replace(',', '')
-        player_points = int(player_points)
+    # print top 10 players' info
+    if command == 'top10':
+        for i, entry in enumerate(entries):
+            if i == 10:
+                break
+            yield yield_rank(entry)
             
+    # print specified ranked players
+    elif 'spec' in command:
+        ranks = command.split(' ')[1].split(',')
+        ranks = list(map(lambda x: int(x), ranks))
+        ranks.sort()
+        print(ranks)
+        for rank in ranks:
+            yield yield_rank(entries[int(rank)-1])
 
-        player_flag = cells[2].div.div.img['alt']
 
-        player_age = cells[4].text.strip()
-        try:
-            player_age = int(player_age)
-        except:
-            pass
 
-        # print(player_rank, player_name, player_points, player_link, player_age, player_flag, height, weight)
-        yield [player_rank, player_name, str(player_points), str(player_age), player_flag, str(height), str(weight)]
-        # df.loc[i] = [player_rank, player_name, player_points, player_link, player_age, player_flag, height, weight ]
-        # break
 
 # # scrape_rank(url)
 # scrape_rank(url)
--- tennis.py
+++ tennis.py
@@ -1,8 +1,11 @@
 # bot.py
 import os
+import time
+import datetime as dt
 
 import discord
-from discord.ext import commands
+import asyncio
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from RankScrape import scrape_rank
 
@@ -10,34 +13,113 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='!tb_')
 
-# @bot.event
-# async def on_ready():
-    
-#     for guild in client.guilds:
-#         if guild.name == GUILD:
-#             break
-#     print(f'{client.user} has connected to Discord!'
-#             # f'{guild.name}(id: {guild.id})'    
-#         )
-#     print(guild.id)
+scrape_url = 'https://www.atptour.com/en/rankings/singles?rankRange=1-5000'
+scrape_url_top100 = 'https://www.atptour.com/en/rankings/singles'
+image_prefix = 'https://www.atptour.com'
+empty_image = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-def format_output(l):
-    txt = "{:<8} {:<25} {:<8} {:<8} {:<8} {:<10} {:<10}" 
-    return txt.format(*l)
+# def format_output(l):
+#     txt = "{:<8} {:<25} {:<8} {:<8} {:<8} {:<10} {:<10}" 
+#     return txt.format(*l)
 
-@bot.command(name='top10rank', help='list top 10 ATP players')
+# print command usage info to log 
+def command_user(ctx):
+    print('{} used the \'{}\' in \'{}\' channel of \'{}\''.format(ctx.author, ctx.command, ctx.channel, ctx.guild))
+    print('^^^time =', time.strftime('%X'))
+    print('message =', ctx.message.content)
+
+# @bot.command(name='top10rank', help='list top 10 ATP players')
+# async def nine_nine(ctx):
+#     command_user(ctx)
+#     response = format_output(['rank', 'name', 'point', 'age', 'country', 'height(cm)', 'weight (lbs)'])
+#     await ctx.send(response)
+#     rank = scrape_rank('https://www.atptour.com/en/rankings/singles', command='top10')
+#     while True:
+#         try:
+#             response = format_output(next(rank))
+#             await ctx.send(response)
+#         except StopIteration:
+#             break
+
+
+# build embed based on initial embed heading and iterable of player info
+async def build_embed(embed, rank):
+    first = True
+    # file= None
+    for rk, name, pts, age, flag, ht, wt, pl, img in rank:
+        if first:
+            if img is None:
+                embed.set_image(url=empty_image)
+            else:
+                embed.set_image(url=image_prefix+img)
+            first = False
+        embed.add_field(name=f'**No.{rk}: {name} ({pts})**', value=f'\n\n\n> [link]({pl})\n> age: {age}\n> flag: {flag}\n> height(cm): {ht}\n> weight(lbs): {wt}\n\n',inline=False)
+    return embed
+
+# Scrape top 10 ATP players
+@bot.command(name='top10', help='list top 10 ATP players')
 async def nine_nine(ctx):
-    print(ctx.author, 'used the rank command')
-    response = format_output(['rank', 'name', 'point', 'age', 'country', 'height(cm)', 'weight (lbs)'])
-    await ctx.send(response)
-    rank = scrape_rank('https://www.atptour.com/en/rankings/singles')
-    for i in range(10):
-        response = format_output(next(rank))
-        await ctx.send(response)
+    command_user(ctx)
+    await ctx.send("extracting...")
+
+    embed = discord.Embed(title=f"__**Current top 10 players:**__", color=0x03f8fc,timestamp= ctx.message.created_at)
+    rank = scrape_rank(scrape_url, command='top10')
+    
+    embed = await build_embed(embed, rank)
+    await ctx.send(embed=embed)
+
+# scrape players based on ranks specified by users
+@bot.command(name='rank', help='\'rank\' x,y,z,... prints player one by one from high rank to low rank')
+async def rank_spec(ctx, rank):
+    command_user(ctx)
+    await ctx.send('formating...')
+
+    embed = discord.Embed(title=f"__**Chosen rank:**__", color=0xff0000,timestamp= ctx.message.created_at)
+    try:
+        rank = scrape_rank(scrape_url, command='spec ' + str(rank))
+        embed = await build_embed(embed, rank)
+        await ctx.send(embed=embed)
+    except:
+        await ctx.send(f"rank number has to be integer: {ctx.message.content}")
+    
+target_channel_ids = [977292185319911467, 688950889738010669, 524687944917712896]
+
+# weekly ranking updates
+@tasks.loop(hours=168)
+async def called_once_a_week():
+    rank = scrape_rank(scrape_url, command='top10')
+
+    embed = discord.Embed(title=f"__**!New top 10 Ranking!**__", color=0x03f8fc)#,timestamp= ctx.message.created_at)
+
+    embed = await build_embed(embed, rank)
+    for id in target_channel_ids:
+        message_channel = bot.get_channel(id)
+        await message_channel.send("!!! WEEKLY RANKING UPDATE !!!")
+        await message_channel.send(embed=embed)
+
+    print("Finished weekly ranking update")
+
+@called_once_a_week.before_loop
+async def before():
+    # loop the whole 7 day (60 sec 60 min 24 hours 7 days)
+    for _ in range(60*60*24*7):  
+        if dt.datetime.utcnow().strftime("%H:%M UTC %a") == "13:30 UTC Mon":
+            print('time for weekly update')
+            return
+
+        # wait some time before another loop. Don't make it more than 60 sec or it will skip
+        await asyncio.sleep(30)
+    # await asyncio.sleep(295200)
+    # print("Finished waiting")
+    # await bot.wait_until_ready()
+
+called_once_a_week.start()
 
 bot.run(TOKEN)
